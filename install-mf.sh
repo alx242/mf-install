4bd154cf802c23e85459978e99779eedab8bea84
Alexander Schüssler <alex@xalx.net>
2019-10-13 19:58:16 +0200

    Ubuntu 18.06 and Children of Morta version

--- install-mf.sh
+++ install-mf.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+#
 
-[ -z "$WINEPREFIX" ] && echo "WINEPREFIX not set" && exit 1
-
-set -e
+GAME_ID="330020"  # Children of morta
+STEAM_ROOT="${HOME}/.steam"
+PROTON_ROOT="${STEAM_ROOT}/steam/steamapps/common/Proton 4.11/dist"
+export PATH="${PROTON_ROOT}/bin:$PATH"  # make "wine" visible
+HOST_LIBS="${STEAM_ROOT}/ubuntu12_32/steam-runtime/amd64"
+export LD_LIBRARY_PATH="${PROTON_ROOT}/lib64:$HOST_LIBS/lib/x86_64-linux-gnu:$HOST_LIBS/usr/lib/x86_64-linux-gnu"
+export WINEPREFIX="${STEAM_ROOT}/steam/steamapps/compatdata/$GAME_ID/pfx"
 
 overrideDll() {
   wine reg add "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v $1 /d native /f
@@ -11,13 +16,12 @@ overrideDll() {
 scriptdir=$(dirname "$0")
 cd "$scriptdir"
 
-cp -v syswow64/* "$WINEPREFIX/drive_c/windows/syswow64"
-cp -v system32/* "$WINEPREFIX/drive_c/windows/system32"
+cp -v syswow64/* "${WINEPREFIX}/drive_c/windows/syswow64"
+cp -v system32/* "${WINEPREFIX}/drive_c/windows/system32"
 
 overrideDll "mf"
 overrideDll "mferror"
 overrideDll "mfplat"
-overrideDll "mfplay"
 overrideDll "mfreadwrite"
 overrideDll "msmpeg2adec"
 overrideDll "msmpeg2vdec"
@@ -27,13 +31,10 @@ export WINEDEBUG="-all"
 
 wine start regedit.exe mf.reg
 wine start regedit.exe wmf.reg
-wine64 start regedit.exe mf.reg
-wine64 start regedit.exe wmf.reg
-
-wine64 regsvr32 msmpeg2vdec.dll
-wine64 regsvr32 msmpeg2adec.dll
-wine64 regsvr32 colorcnv.dll
-
 wine regsvr32 msmpeg2vdec.dll
 wine regsvr32 msmpeg2adec.dll
-wine regsvr32 colorcnv.dll
+
+wine64 start regedit.exe mf.reg
+wine64 start regedit.exe wmf.reg
+wine64 regsvr32 msmpeg2vdec.dll
+wine64 regsvr32 msmpeg2adec.dll
